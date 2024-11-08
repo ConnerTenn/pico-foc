b6e65e828860946c57f96ca6bad5c1dd77c0a931
Conner Tenn <connertenn@gmail.com>
2024-11-08 00:57:26 -0500

    PWM is working for all 3 slices
    Figured out how to synchronize all 3 PWM signals

--- src/pwm.zig
+++ src/pwm.zig
@@ -2,81 +2,76 @@ const bldc = @import("bldc.zig");
 const stdio = bldc.stdio;
 const csdk = bldc.csdk;
 
-pub const Pwm = struct {
-    const Self = @This();
-
-    slice: c_uint,
-    channel: c_uint,
-
-    pub fn create(pin: u32) Self {
-        csdk.gpio_set_function(pin, csdk.GPIO_FUNC_PWM);
-
-        return Self{
-            .slice = csdk.pwm_gpio_to_slice_num(pin),
-            .channel = csdk.pwm_gpio_to_channel(pin),
-        };
-    }
-
-    pub inline fn setLevel(self: Self, level: u16) void {
-        csdk.pwm_set_chan_level(self.slice, self.channel, level);
-    }
-};
-
 pub const PwmPair = struct {
     const Self = @This();
 
-    pwm_a: Pwm,
-    pwm_b: Pwm,
-    config: csdk.pwm_config,
+    slice: c_uint,
 
-    pub fn create(pin_a: u32, pin_b: u32) !Self {
+    pub fn create(slice: u8) !Self {
+        const self = Self{
+            .slice = slice,
+        };
+
+        return self;
+    }
+
+    pub fn initialize(self: Self) void {
         var config = csdk.pwm_get_default_config();
 
         csdk.pwm_config_set_output_polarity(&config, false, true);
         csdk.pwm_config_set_clkdiv_int(&config, 255);
 
-        const self = Self{
-            .pwm_a = Pwm.create(pin_a),
-            .pwm_b = Pwm.create(pin_b),
-            .config = config,
-        };
+        csdk.pwm_init(self.slice, &config, false);
 
-        if (self.pwm_a.slice != self.pwm_b.slice or self.pwm_a.channel == self.pwm_b.channel) {
-            return error.incompatible_pins;
-        }
-
-        return self;
-    }
-
-    pub fn enable(self: *Self) void {
-        csdk.pwm_init(self.pwm_a.slice, &self.config, true);
+        csdk.pwm_set_counter(self.slice, 0);
     }
 
     pub fn disable(self: Self) void {
-        csdk.pwm_set_enabled(self.pwm_a.slice, false);
+        csdk.pwm_set_enabled(self.slice, false);
     }
 
     pub inline fn setLevel(self: Self, level: u16) void {
-        csdk.pwm_set_both_levels(self.pwm_a.slice, level, level);
+        csdk.pwm_set_both_levels(self.slice, level, level);
     }
 };
 
+const PwmSlices = struct {
+    const slice_u: u8 = 4;
+    const slice_v: u8 = 6;
+    const slice_w: u8 = 7;
+};
+
 pub fn demo() noreturn {
-    var pwm_u = PwmPair.create(8, 9) catch |err| {
-        stdio.print("Error {}", .{err});
+    const pwm_u = PwmPair.create(PwmSlices.slice_u) catch |err| {
+        stdio.print("Error {}\n", .{err});
         @panic("dead");
     };
-    // const pwm_v = PwmPair.create(12, 13) catch |err| {
-    //     stdio.print("Error {}", .{err});
-    // };
-    // _ = pwm_v; // autofix
-    // const pwm_w = PwmPair.create(14, 15) catch |err| {
-    //     stdio.print("Error {}", .{err});
-    // };
-    // _ = pwm_w; // autofix
+    const pwm_v = PwmPair.create(PwmSlices.slice_v) catch |err| {
+        stdio.print("Error {}\n", .{err});
+        @panic("dead");
+    };
+    const pwm_w = PwmPair.create(PwmSlices.slice_w) catch |err| {
+        stdio.print("Error {}\n", .{err});
+        @panic("dead");
+    };
 
-    pwm_u.enable();
+    csdk.gpio_set_function(8, csdk.GPIO_FUNC_PWM);
+    csdk.gpio_set_function(9, csdk.GPIO_FUNC_PWM);
+    csdk.gpio_set_function(12, csdk.GPIO_FUNC_PWM);
+    csdk.gpio_set_function(13, csdk.GPIO_FUNC_PWM);
+    csdk.gpio_set_function(14, csdk.GPIO_FUNC_PWM);
+    csdk.gpio_set_function(15, csdk.GPIO_FUNC_PWM);
+
+    pwm_u.initialize();
+    pwm_v.initialize();
+    pwm_w.initialize();
+
     pwm_u.setLevel(0xffff / 2);
+    pwm_v.setLevel(0xffff / 2);
+    pwm_w.setLevel(0xffff / 2);
+
+    //Enable all pwm signals at once
+    csdk.pwm_set_mask_enabled((1 << PwmSlices.slice_u) | (1 << PwmSlices.slice_v) | (1 << PwmSlices.slice_w));
 
     while (true) {}
 }
