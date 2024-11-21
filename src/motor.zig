8c7c3f82c379dd92e49e36ab7f4f435a56407b82
Conner Tenn <connertenn@gmail.com>
2024-11-20 22:59:07 -0500

    Added more comprehensive calibration routine

--- src/motor.zig
+++ src/motor.zig
@@ -24,17 +24,20 @@ pub const Motor = struct {
         };
     }
 
+    const num_calibration_samples = 128;
+
     driver: pwm.PwmDriver,
     windings_per_rotation: u8,
 
     sensor: bldc.sensor.LIS3MDL,
+
+    calibration_data: [num_calibration_samples]f32 = undefined,
 
     target: Parameters(?f32) = .{ .angle = null, .velocity = null, .torque = null, .acceleration = null },
     limits: Parameters(?f32) = .{ .angle = null, .velocity = null, .torque = null, .acceleration = null },
     state: Parameters(f32) = .{ .angle = 0, .velocity = 0, .torque = 0, .acceleration = 0 },
 
     sensor_angle: f32 = 0,
-    sensor_angle_bias: f32,
 
     last_time_us: csdk.absolute_time_t = 0,
 
@@ -43,7 +46,6 @@ pub const Motor = struct {
             .driver = pwm.PwmDriver.create(u_axis_slice, v_axis_slice, w_axis_slice),
             .windings_per_rotation = windings_per_rotation,
             .sensor = sensor,
-            .sensor_angle_bias = 0,
         };
     }
 
@@ -56,15 +58,60 @@ pub const Motor = struct {
         //Settling time before measuring the angle bias
         csdk.sleep_ms(1000);
 
-        self.sensor_angle_bias = self.sensor.getAngle();
-        stdio.print("sensor angle bias: {d:.3}\n", .{self.sensor_angle_bias / math.tau});
+        self.calibrate();
 
         //Update the last time
         self.last_time_us = csdk.get_absolute_time();
     }
 
+    pub fn calibrate(self: *Self) void {
+        stdio.print("Running Calibration...\n", .{});
+
+        //Loop through every sample
+        for (0..num_calibration_samples) |sample_idx| {
+            //Drive to the target angle
+            const target_angle = tau * @as(f32, @floatFromInt(sample_idx)) / @as(f32, @floatFromInt(num_calibration_samples));
+
+            self.setTorque(1.0, 0.0, target_angle);
+            csdk.sleep_ms(200);
+
+            //Collect a number of samples and average them
+            const num_samples = 10;
+            var measured_angle: f32 = 0;
+            for (0..num_samples) |_| {
+                measured_angle += self.sensor.getAngle();
+            }
+            measured_angle = measured_angle / @as(f32, @floatFromInt(num_samples));
+
+            self.calibration_data[sample_idx] = target_angle - measured_angle;
+        }
+
+        stdio.print("Calibration samples:\n", .{});
+        for (0..num_calibration_samples) |sample_idx| {
+            const target_angle = tau * @as(f32, @floatFromInt(sample_idx)) / @as(f32, @floatFromInt(num_calibration_samples));
+            stdio.print("{d: >6.3} rad: {d: >6.3}\n", .{ target_angle, self.calibration_data[sample_idx] });
+        }
+    }
+
     pub fn setTorque(self: Self, direct_torque: f32, tangent_torque: f32, angle: f32) void {
         self.driver.setTorque(direct_torque, tangent_torque, angle * @as(f32, @floatFromInt(self.windings_per_rotation)));
+    }
+
+    pub fn getAngle(self: Self) f32 {
+        const raw_angle = self.sensor.getAngle();
+        // stdio.print("raw_angle:{d} ", .{raw_angle});
+
+        //Offset to apply so that the selected sample is in the middle of the angle range
+        const offset_for_centered_sample = tau / @as(f32, @floatFromInt(num_calibration_samples));
+        const sample_bin = @as(f32, @floatFromInt(num_calibration_samples)) * raw_angle / tau + offset_for_centered_sample;
+        const sample_idx = @mod(@as(u16, @intFromFloat(sample_bin)), num_calibration_samples);
+        // stdio.print("sample_idx:{} ", .{sample_idx});
+
+        //Get the compesated angle using the calibration data
+        const compensated_angle = raw_angle + self.calibration_data[sample_idx];
+
+        // stdio.print("compensated_angle:{d}\n", .{compensated_angle});
+        return bldc.mod(f32, compensated_angle, tau, .regular);
     }
 
     pub fn setPosition(self: Self, angle: f32) void {
@@ -82,7 +129,7 @@ pub const Motor = struct {
         const delta_time_us = current_time_us - self.last_time_us;
         const delta_time_s: f32 = @as(f32, @floatFromInt(delta_time_us)) / (1000.0 * 1000.0);
 
-        const new_sensor_angle = bldc.mod(f32, self.sensor.getAngle() - self.sensor_angle_bias, tau, .regular);
+        const new_sensor_angle = self.getAngle();
         //Smoothing?
         self.sensor_angle = self.sensor_angle * 0.0 + new_sensor_angle * 1.0;
 
