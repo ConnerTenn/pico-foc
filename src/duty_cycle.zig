64250fc7d16ad42a50442ccec5b520bcf7895f62
Conner Tenn <connertenn@gmail.com>
2025-01-19 23:59:35 -0500

    Now using PIO objects from pico module

--- src/duty_cycle.zig
+++ src/duty_cycle.zig
@@ -4,6 +4,7 @@ const math = std.math;
 const pico = @import("pico");
 const csdk = pico.csdk;
 const stdio = pico.stdio;
+const pio = pico.pio;
 
 const bldc = @import("bldc.zig");
 const foc = bldc.foc;
@@ -12,69 +13,54 @@ const duty_cycle_pio = @cImport({
     @cInclude("duty_cycle.pio.h");
 });
 
-pub const Pio = struct {
-    const Self = @This();
-
-    pio_obj: csdk.PIO,
-    state_machine: c_uint,
-    offset: c_uint,
-    gpio_base: c_uint,
-    gpio_count: c_uint,
-
-    pub fn create(program: *const csdk.pio_program_t, gpio_base: c_uint, gpio_count: c_uint) Self {
-        var state_machine: c_uint = undefined;
-        var pio_obj: csdk.PIO = undefined;
-        var offset: c_uint = undefined;
-        _ = csdk.pio_claim_free_sm_and_add_program_for_gpio_range(program, &pio_obj, &state_machine, &offset, gpio_base, gpio_count, true);
-
-        const self = Self{
-            .pio_obj = pio_obj,
-            .state_machine = state_machine,
-            .offset = offset,
-            .gpio_base = gpio_base,
-            .gpio_count = gpio_count,
-        };
-
-        return self;
-    }
-};
-
 pub const DutyCycle = struct {
     const Self = @This();
 
     // pio_obj: csdk.PIO,
     // state_machine: c_uint,
-    pio_high: Pio,
-    pio_low: Pio,
+    pio_high: pio.Pio,
+    pio_low: pio.Pio,
 
-    pub fn create(gpio_base: c_uint, gpio_count: c_uint) Self {
+    pub fn create(gpio_base: pico.gpio.Pin, gpio_count: pico.gpio.Pin.Count) Self {
         // _ = csdk.pio_claim_free_sm_and_add_program_for_gpio_range(program, &pio_obj, &state_machine, &offset, gpio_base, gpio_count, true);
         // const pio = Pio.create(&csdk.duty_cycle_program, gpio_base, gpio_count);
 
         return Self{
-            .pio_high = Pio.create(@ptrCast(&duty_cycle_pio.high_cycle_program), gpio_base, gpio_count),
-            .pio_low = Pio.create(@ptrCast(&duty_cycle_pio.low_cycle_program), gpio_base, gpio_count),
+            .pio_high = pio.Pio.create(
+                @ptrCast(&duty_cycle_pio.high_cycle_program),
+                @ptrCast(&duty_cycle_pio.high_cycle_program_get_default_config),
+                gpio_base,
+                gpio_count,
+            ),
+            .pio_low = pio.Pio.create(
+                @ptrCast(&duty_cycle_pio.low_cycle_program),
+                @ptrCast(&duty_cycle_pio.high_cycle_program_get_default_config),
+                gpio_base,
+                gpio_count,
+            ),
         };
     }
 
     pub fn init(self: *Self) void {
         //High Cycle
         //Configure the pin direction
-        _ = csdk.pio_sm_set_consecutive_pindirs(self.pio_high.pio_obj, self.pio_high.state_machine, self.pio_high.gpio_base, self.pio_high.gpio_count, false);
+        self.pio_high.setConsecutivePinDirs(self.pio_high.gpio_base, self.pio_high.gpio_count, false);
         //Configure the state machine
-        var high_cycle_state_machine_config: csdk.pio_sm_config = @bitCast(duty_cycle_pio.high_cycle_program_get_default_config(self.pio_high.offset));
-        csdk.sm_config_set_jmp_pin(&high_cycle_state_machine_config, self.pio_high.gpio_base);
+        var pio_high_config = self.pio_high.getDefaultConfig();
+        pio_high_config.setJmpPin(self.pio_high.gpio_base);
         //Start the state machine
-        _ = csdk.pio_sm_init(self.pio_high.pio_obj, self.pio_high.state_machine, self.pio_high.offset, &high_cycle_state_machine_config);
-        csdk.pio_sm_set_enabled(self.pio_high.pio_obj, self.pio_high.state_machine, true);
+        self.pio_high.init(pio_high_config);
+        self.pio_high.enable();
 
         //Low Cycle
+        //Configure the pin direction
+        self.pio_low.setConsecutivePinDirs(self.pio_low.gpio_base, self.pio_low.gpio_count, false);
         //Configure the state machine
-        var low_cycle_state_machine_config: csdk.pio_sm_config = @bitCast(duty_cycle_pio.low_cycle_program_get_default_config(self.pio_low.offset));
-        csdk.sm_config_set_jmp_pin(&low_cycle_state_machine_config, self.pio_low.gpio_base);
+        var pio_low_config = self.pio_low.getDefaultConfig();
+        pio_low_config.setJmpPin(self.pio_low.gpio_base);
         //Start the state machine
-        _ = csdk.pio_sm_init(self.pio_low.pio_obj, self.pio_low.state_machine, self.pio_low.offset, &low_cycle_state_machine_config);
-        csdk.pio_sm_set_enabled(self.pio_low.pio_obj, self.pio_low.state_machine, true);
+        self.pio_low.init(pio_low_config);
+        self.pio_low.enable();
     }
 
     pub fn readDutyCycle(self: *Self) f32 {
